65978c2e7efee018fca8d76386bb072edc2f5f54
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-08-06 18:01:48 +0000

    feat: add display for types

--- src/main.rs
+++ src/main.rs
@@ -1241,16 +1241,117 @@ impl TermState {
     }
 }
 
+impl Pattern {
+    // Returns a debug representation of the pattern.
+    pub fn display<'state>(&self, state: &'state TermState) -> PatternDisplay<'state> {
+        PatternDisplay {
+            state,
+            pattern: *self,
+        }
+    }
+}
+
+pub struct PatternDisplay<'state> {
+    pub state: &'state TermState,
+    pub pattern: Pattern,
+}
+
+impl Display for PatternDisplay<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.state.patterns.get(self.pattern) {
+            PatternKind::Error => write!(f, "error"),
+            PatternKind::Variable(name) => write!(f, "{name}"),
+            PatternKind::Constructor(constructor, patterns) => {
+                write!(
+                    f,
+                    "({constructor} {})",
+                    patterns
+                        .into_iter()
+                        .map(|pattern| pattern.display(self.state))
+                        .fold("".to_string(), |acc, next| {
+                            if acc.is_empty() {
+                                format!("{next}")
+                            } else {
+                                format!("{acc} {next}")
+                            }
+                        })
+                )
+            }
+        }
+    }
+}
+
+impl Term {
+    // Returns a debug representation of the term.
+    pub fn display<'state>(&self, state: &'state TermState) -> TermDisplay<'state> {
+        TermDisplay { state, term: *self }
+    }
+}
+
+pub struct TermDisplay<'state> {
+    pub state: &'state TermState,
+    pub term: Term,
+}
+
+impl Display for TermDisplay<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.state.terms.get(self.term) {
+            TermKind::Error => write!(f, "error"),
+            TermKind::Type(level) => write!(f, "type {level}"),
+            TermKind::Number(n) => write!(f, "{n}"),
+            TermKind::String(string) => write!(f, "\"{string}\""),
+            TermKind::Constructor(constructor) => write!(f, "{constructor}"),
+            TermKind::Hole(hole) => write!(f, "?{}", hole.name.unwrap_or("_".into())),
+            TermKind::Ann(value, against) => write!(
+                f,
+                "{} : {}",
+                value.display(self.state),
+                against.display(self.state)
+            ),
+            TermKind::Lambda(parameter, value) => write!(
+                f,
+                "\\{}. {}",
+                parameter.display(self.state),
+                value.display(self.state)
+            ),
+            TermKind::Apply(callee, arg) => write!(
+                f,
+                "{} {}",
+                callee.display(self.state),
+                arg.display(self.state)
+            ),
+            TermKind::Let(name, value, expr) => write!(
+                f,
+                "let {} = {} in {}",
+                name.display(self.state),
+                value.display(self.state),
+                expr.display(self.state)
+            ),
+            TermKind::Pi(name, domain, codomain) if name == "_" => write!(
+                f,
+                "{} -> {}",
+                domain.display(self.state),
+                codomain.display(self.state)
+            ),
+            TermKind::Pi(name, domain, codomain) => write!(
+                f,
+                "({name} : {}) -> {}",
+                domain.display(self.state),
+                codomain.display(self.state)
+            ),
+        }
+    }
+}
+
 fn main() {
     let mut state = TermState::default();
     let ast = state.parse([
-        // Source code
         "// simple file",
         "data Bool : Type where",
-        "  True : Bool",
+        "  True  : Bool",
         ", False : Bool",
         "",
-        "fun and : (x: Bool) -> Bool -> Bool",
+        "fun and : Bool -> Bool -> Bool",
         "        | True, True   = True",
         "        | _,    False  = False",
         "        | _,    _      = False",
